a49dbe30aa46a058b29bb34de0b4d429dcf23caa
VVMaestro <bmaecmpo@gmail.com>
2023-11-22 10:59:43 +0500

    feat: adds interactivity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aquablog.netlify.app"
+  site: "https://aquablog.netlify.app",
+  integrations: [preact()]
 });
